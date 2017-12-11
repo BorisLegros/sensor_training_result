33e333fe87dc8d6fa93246c92df5cc1ade53aa6b
Boris <bo.legros@laposte.net>
2017-12-11 15:23:12 +0100

    finale package

--- scripts/pubCloud.py
+++ scripts/pubCloud.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
-import rospy
-from sensor_msgs import PointCloud
+import roslib; roslib.load_manifest('laser_assembler')
+import rospy; from laser_assembler.srv import *
+from sensor_msgs.msg import PointCloud
 from laser_assembler import AssembleScans
-
 rospy.init_node('pubCloud')
-pub = rospy.Publsher('~/cloud', PointCloud)
+pub = rospy.Publisher('~/cloud', PointCloud, queue_size=10)
 rate = rospy.Rate(5000)
+msg = AssembleScans()
 
 while not rospy.is_shutdown():
 	rospy.wait_for_service('assemble_scans')
-	try:
-		assScan = rospy.ServiceProxy('assemble_scans', AssembleScans)
-		msg = PointCloud()
-		msg = assemble_scans(0, 5000)
-		pub.publish(msg)
+	assemble_scans = rospy.ServiceProxy('assemble_scans', AssembleScans)
+	msg = assemble_scans(rospy.Time(0,0), rospy.get_rostime())
+	pub.publish(msg)
 	rate.sleep()
